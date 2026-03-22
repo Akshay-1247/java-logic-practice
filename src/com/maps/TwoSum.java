2f9d4779d4999c746a4e40a97dc6028da82d68c4
Akshay <akshayh1247@gmail.com>
2026-03-22 17:17:48 +0530

    Added Two Sum problem

---
+++ src/com/maps/TwoSum.java
@@ -0,0 +1,32 @@
+package com.maps;
+
+import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
+
+public class TwoSum {
+
+	public static void main(String[] args) {
+
+		int arr[] = { 1, 2, 3, 4, 5, 6 };
+		int target = 9;
+
+		int result[] = calculateTwoSum(arr, target);
+		System.out.println(result[0] + " " + result[1]);
+	}
+
+	public static int[] calculateTwoSum(int arr[], int target) {
+
+		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+		for (int i = 0; i < arr.length; i++) {
+			int compliment = target - arr[i];
+			if (map.containsKey(compliment)) {
+				int result[] = { map.get(compliment), i };
+				return result;
+			}
+			map.put(arr[i], i);
+		}
+		return null;
+
+	}
+}
